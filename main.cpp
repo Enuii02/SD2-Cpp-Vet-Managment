963d03c27859695a2ea8cd780f163832104159d5
W04 <44273988+W04@users.noreply.github.com>
2025-04-10 12:54:52 +0100

    com off

--- main.cpp
+++ main.cpp
@@ -18,24 +18,24 @@ int main() {
     // For Testing, delete later
     //-------------------------------------------------
 
-    Pet myPet("Buddy", "john_doe", "2024-05-01, 2025-01-15", "2020-08-12", "Golden Retriever");
+    // Pet myPet("Buddy", "john_doe", "2024-05-01, 2025-01-15", "2020-08-12", "Golden Retriever");
 
-    User user1("john_doe", "admin", "secure_password", "John Doe", "john.doe@example.com", "555-1234");
+    // User user1("john_doe", "admin", "secure_password", "John Doe", "john.doe@example.com", "555-1234");
 
-    User user2("jane_smith", "staff", "password123", "Jane Smith", "jane.smith@email.com", "555-5678");
+    // User user2("jane_smith", "staff", "password123", "Jane Smith", "jane.smith@email.com", "555-5678");
 
-    User user3("alex_brown", "staff", "alexpass456", "Alex Brown", "alex.brown@mail.net", "555-8765");
+    // User user3("alex_brown", "staff", "alexpass456", "Alex Brown", "alex.brown@mail.net", "555-8765");
 
 
     // Save save;
     // save.saveUser("john_doe", "admin", "secure_password", "John Doe", "john.doe@example.com", "555-1234");
 
-    View view;
-    view.viewAllUsers("Data/staffacc.txt");
+    // View view;
+    // view.viewAllUsers("Data/staffacc.txt");
     // view.viewAllPets("Data/pets.txt");
 
-    Update update;
-    update.updateUser("john_doe", "Data/staffacc.txt");
+    // Update update;
+    // update.updateUser("john_doe", "Data/staffacc.txt");
 
     //-------------------------------------------------
 
